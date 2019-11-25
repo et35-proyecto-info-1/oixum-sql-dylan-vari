153a8d0c7f48ce0e6e9b6ec9f5ea197cb34fe166
DylanToscano <49658747+DylanToscano@users.noreply.github.com>
2019-11-24 21:59:34 -0300

    la otra

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE IF NOT EXISTS test;
-
 CREATE TABLE IF NOT EXISTS test.estudiantes (
 	id INT NOT NULL AUTO_INCREMENT,
 	nombre varchar(100) NOT NULL,
@@ -7,3 +6,10 @@ CREATE TABLE IF NOT EXISTS test.estudiantes (
 	fechaNacimiento DATE NOT NULL,
 	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
 );
+
+INSERT INTO test.estudiantes (nombre, comidaPreferida, fechaNacimiento)
+VALUES 
+  ('Federico Aloi', 'Panqueques de arroz', '1991-10-30'),
+  ('Andrea Sierra Bueno', 'Puré de papas con huevo revuelto', '1985-08-30'),
+  ('Fernando Cazas', 'Hamburguesas de lentejas', '1963-02-12'),
+  ('Marta Lucero', 'Conejo a la cacerola', '1938-01-24');
